7b872ee7e2f0ba9b309d86089b3417108cc295c7
ququzone <xueping.yang@gmail.com>
2023-04-11 07:22:20 +0800

    add new op from json

--- signer/signer.go
+++ signer/signer.go
@@ -7,6 +7,6 @@ func NewSigner() (*Signer, error) {
 	return &Signer{}, nil
 }
 
-func (s *Signer) Eth_signVerifyingPaymaster() (string, error) {
+func (s *Signer) Eth_signVerifyingPaymaster(op map[string]any) (string, error) {
 	return "hello", nil
 }
--- types/op.go
+++ types/op.go
@@ -1,11 +1,17 @@
 package types
 
 import (
+	"encoding/hex"
+	"errors"
 	"math/big"
+	"reflect"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/go-playground/validator"
+	"github.com/mitchellh/mapstructure"
 )
 
 var (
@@ -24,8 +30,10 @@ var (
 	}
 
 	UserOpType, _ = abi.NewType("tuple", "op", UserOpPrimitives)
+	UserOpArr, _  = abi.NewType("tuple[]", "ops", UserOpPrimitives)
 
-	UserOpArr, _ = abi.NewType("tuple[]", "ops", UserOpPrimitives)
+	validate = validator.New()
+	onlyOnce = sync.Once{}
 )
 
 func getAbiArgs() abi.Arguments {
@@ -87,4 +95,101 @@ func (op *UserOperation) Pack() []byte {
 	enc := hexutil.Encode(packed)
 	enc = "0x" + enc[66:]
 	return (hexutil.MustDecode(enc))
+}
+func exactFieldMatch(mapKey, fieldName string) bool {
+	return mapKey == fieldName
+}
+
+func decodeOpTypes(
+	f reflect.Kind,
+	t reflect.Kind,
+	data interface{}) (interface{}, error) {
+	// String to common.Address conversion
+	if f == reflect.String && t == reflect.Array {
+		return common.HexToAddress(data.(string)), nil
+	}
+
+	// String to big.Int conversion
+	if f == reflect.String && t == reflect.Struct {
+		n := new(big.Int)
+		n, ok := n.SetString(data.(string), 0)
+		if !ok {
+			return nil, errors.New("bigInt conversion failed")
+		}
+		return n, nil
+	}
+
+	// Float64 to big.Int conversion
+	if f == reflect.Float64 && t == reflect.Struct {
+		n, ok := data.(float64)
+		if !ok {
+			return nil, errors.New("bigInt conversion failed")
+		}
+		return big.NewInt(int64(n)), nil
+	}
+
+	// String to []byte conversion
+	if f == reflect.String && t == reflect.Slice {
+		byteStr := data.(string)
+		if len(byteStr) < 2 || byteStr[:2] != "0x" {
+			return nil, errors.New("not byte string")
+		}
+
+		b, err := hex.DecodeString(byteStr[2:])
+		if err != nil {
+			return nil, err
+		}
+		return b, nil
+	}
+
+	return data, nil
+}
+
+func validateAddressType(field reflect.Value) interface{} {
+	value, ok := field.Interface().(common.Address)
+	if !ok || value == common.HexToAddress("0x") {
+		return nil
+	}
+
+	return field
+}
+
+func validateBigIntType(field reflect.Value) interface{} {
+	value, ok := field.Interface().(big.Int)
+	if !ok || value.Cmp(big.NewInt(0)) == -1 {
+		return nil
+	}
+
+	return field
+}
+
+func New(data map[string]any) (*UserOperation, error) {
+	var op UserOperation
+
+	// Convert map to struct
+	config := &mapstructure.DecoderConfig{
+		DecodeHook: decodeOpTypes,
+		Result:     &op,
+		ErrorUnset: true,
+		MatchName:  exactFieldMatch,
+	}
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return nil, err
+	}
+	if err := decoder.Decode(data); err != nil {
+		return nil, err
+	}
+
+	// Validate struct
+	onlyOnce.Do(func() {
+		validate.RegisterCustomTypeFunc(validateAddressType, common.Address{})
+		validate.RegisterCustomTypeFunc(validateBigIntType, big.Int{})
+	})
+	err = validate.Struct(op)
+	if err != nil {
+		return nil, err
+	}
+
+	return &op, nil
 }
